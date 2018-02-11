fc317edb75ccd73648148a4c02af14ea067b4d66
Andrés Estévez <aecostas@gmail.com>
2018-02-11 23:30:27 +0100

    First commit

---
+++ constants.go
@@ -0,0 +1,16 @@
+// Go supports _constants_ of character, string, boolean,
+// and numeric values.
+
+package main
+
+import "fmt"
+
+// `const` declares a constant value.
+const s string = "constant"
+
+func main() {
+	for {
+	   fmt.Println("loop")
+	   break
+   }
+}
