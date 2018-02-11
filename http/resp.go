04be3a5b8d3b26750830902fa3f4bb240451e647
Andrés Estévez <aecostas@gmail.com>
2018-02-12 00:10:57 +0100

    Iterating over XML response

--- http/resp.go
+++ http/resp.go
@@ -2,12 +2,30 @@ package main
 
 import("fmt"
 "net/http"
-"io/ioutil")
+"io/ioutil"
+"encoding/xml")
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
 
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	string_body := string(bytes)
-	fmt.Println(string_body)
 	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	for _, Location := range s.Locations {
+		fmt.Printf("\n%s", Location)
+	}
 }
