357a667a5297da45b893b6706958e83ab2edac18
Andres Estevez Costas <aestevez@gradiant.org>
2018-02-13 17:33:00 +0100

    Parsing XML

--- http/resp.go
+++ http/resp.go
@@ -11,14 +11,20 @@ type SitemapIndex struct {
 
 type News struct {
 	Titles []string `xml:"url>news>title"`
-	Keywords []string `xml:"url>news>Keywords"`
+	Keywords []string `xml:"url>news>keywords"`
 	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
 }
 
 func main() {
 	var s SitemapIndex
 	var n News
 
+	news_map := make(map[string]NewsMap)
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
@@ -30,5 +36,15 @@ func main() {
 		resp, _ := http.Get(Location)
 		bytes, _ := ioutil.ReadAll(resp.Body)
 		xml.Unmarshal(bytes, &n)
+
+		for idx, _ := range n.Titles{
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
+	}
+
+	for idx, data := range news_map {
+		fmt.Println("\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
 	}
 }
